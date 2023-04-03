81dfb769f1fe9bf658c307560f86999a7ff7ed28
Natnael T. Yilma <90861780+TaduJR@users.noreply.github.com>
2023-04-03 16:00:38 +0300

    Create 3. Domino Piling CodeForces.js

---
+++ 3. Domino Piling CodeForces.js
@@ -0,0 +1,7 @@
+// Challange Link: https://codeforces.com/problemset/problem/50/A
+// Note: This code only runs on codeforces javascript runtime enviroment
+
+const inputArr = readline().split(' ');
+const m = inputArr[0];
+const n = inputArr[1];
+print(Math.trunc((m * n) / 2));
